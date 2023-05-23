ec7d1ebfa9d5bd35f713dd5e3ad5f6382382b000
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-23 02:02:36 -0300

    Base del proyecto

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+const  App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
